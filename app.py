0420fb58fbf584a803e0baa8392ebf52c8c5f55b
TeresaFranc <145763618+TeresaFranc@users.noreply.github.com>
2025-04-18 19:44:28 +0000

    api1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/v1.0/predict', methods=['GET', 'POST'])
+@app.route('/api/v1.0/predict', methods=['GET'])
 def predict():
     try:
         num1 = float(request.args.get('num1', 0))
